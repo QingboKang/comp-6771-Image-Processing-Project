8d939784994db1b93b1bb3d604aef0f6f1eceb14
qdsclove <qdsclove@163.com>
2017-11-16 21:59:37 -0500

    preliminary finish pre-processing

--- code/preProcessing.m
+++ code/preProcessing.m
@@ -6,19 +6,21 @@ if numel(img_size)>2
     orig_img = rgb2gray(orig_img);
 end
 
-
 % adjust image
 adjust_img = imadjust(orig_img);
 
-% min max value
-min_value1 = min(orig_img(:))
-max_value1 = max(orig_img(:))
+% extract background image using the gray-level morphological closing
+% operation
+SE = strel('square',5);
+bg_img = imclose(adjust_img, SE);
 
-min_value2 = min(adjust_img(:))
-max_value2 = max(adjust_img(:))
+% subtracting the original from the background one, obtained contrast image
+contrast_img = imsubtract(bg_img, orig_img);
 
-
-
+imshow(orig_img), title('original');
+figure, imshow(adjust_img), title('adjust');
+figure, imshow(bg_img), title('background');
+figure, imshow(contrast_img), title('contrast');
 
 end
 
